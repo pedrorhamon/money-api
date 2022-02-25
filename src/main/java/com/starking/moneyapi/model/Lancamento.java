45363826b8f62f23b94c9a323552a0893b7e4b75
pedrorhamon <pedrorhamon16@gmail.com>
2022-02-24 21:33:38 -0300

    criando a entidade lancamento e o tipo enum

---
+++ src/main/java/com/starking/moneyapi/model/Lancamento.java
@@ -0,0 +1,63 @@
+package com.starking.moneyapi.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.starking.moneyapi.model.enums.TipoLancamentoEnum;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.EqualsAndHashCode.CacheStrategy;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "lancamento")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(cacheStrategy = CacheStrategy.LAZY)
+public class Lancamento {
+	
+	@Column(name = "id")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long codigo;
+	
+	@Column(name = "descricao")
+	private String descricao;
+	
+	@Column(name = "dataLancamento")
+	private LocalDate dataLancamento;
+	
+	@Column(name = "dataPagamento")
+	private LocalDate dataPagamento;
+	
+	@Column(name = "valor")
+	private BigDecimal valor;
+	
+	@Column(name = "observacao")
+	private String observacao;
+	
+	@Enumerated(EnumType.STRING)
+	private TipoLancamentoEnum tipoLancamentoEnum;
+	
+	@ManyToOne
+	@JoinColumn(name = "codigo_categoria")
+	private Categoria categoria;
+	
+	@ManyToOne
+	@JoinColumn(name = "codigo_pessoa")
+	private Pessoa pessoa;
+}
---
+++ src/main/java/com/starking/moneyapi/model/enums/TipoLancamentoEnum.java
@@ -0,0 +1,6 @@
+package com.starking.moneyapi.model.enums;
+
+public enum TipoLancamentoEnum {
+	RECEITA, 
+	DESPESA
+}
