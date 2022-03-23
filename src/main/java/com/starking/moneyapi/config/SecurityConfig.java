87c874d65df72fc3e27b9691a6ca46b5103d74a6
pedrorhamon <pedrorhamon16@gmail.com>
2022-03-22 22:30:31 -0300

    criando parte inicial da seguranca

--- src/main/java/com/starking/moneyapi/config/SecurityConfig.java
+++ src/main/java/com/starking/moneyapi/config/SecurityConfig.java
@@ -1,33 +1,31 @@
 package com.starking.moneyapi.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 @EnableWebSecurity
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig extends OAuth2ResourceServerAutoConfiguration{
 	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	@Autowired
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
 		.withUser("admin")
-		.password("admin").roles("ROle");
+		.password("admin").roles("Role");
 	}
 	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 		.antMatchers("/categorias")
 		.permitAll()
 		.anyRequest().authenticated()
 		.and()
-		.httpBasic().and()
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 		.csrf().disable();
 	}
-
 }
