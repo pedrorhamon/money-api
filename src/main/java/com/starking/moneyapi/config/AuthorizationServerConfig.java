0326b4977037b46440162914e541fec92df23fda
pedrorhamon <pedrorhamon16@gmail.com>
2022-03-22 22:45:28 -0300

    Seguration basic

--- src/main/java/com/starking/moneyapi/config/AuthorizationServerConfig.java
+++ src/main/java/com/starking/moneyapi/config/AuthorizationServerConfig.java
@@ -1,16 +1,10 @@
 package com.starking.moneyapi.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-
 import com.nimbusds.oauth2.sdk.as.AuthorizationServerConfigurationRequest;
 import com.nimbusds.oauth2.sdk.id.Issuer;
 
 public class AuthorizationServerConfig extends AuthorizationServerConfigurationRequest {
 	
-	@Autowired
-	private AuthenticationManager manager;
-
 	public AuthorizationServerConfig(Issuer issuer) {
 		super(issuer);
 	}
