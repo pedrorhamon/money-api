997db14e4ea83b23b628c48a12b62f04f2df6a5e
pedrorhamon <pedrorhamon16@gmail.com>
2022-03-25 20:14:11 -0300

    Authorization

--- src/main/java/com/starking/moneyapi/config/AuthorizationServerConfig.java
+++ src/main/java/com/starking/moneyapi/config/AuthorizationServerConfig.java
@@ -4,9 +4,11 @@ import java.util.Locale;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
+@Configuration
 public class AuthorizationServerConfig {
 	
 	@Bean
